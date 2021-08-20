f4b18fe83d1e1a661023a10dfcf9c242f4a71f96
proazel <proazelhwan@gmail.com>
2021-08-20 14:27:05 +0900

    2개 페이지 호선 추가

--- front/components/menu/Search.jsx
+++ front/components/menu/Search.jsx
@@ -20,20 +20,23 @@ trainLineNm 도착지방면
 */
 
 export default function Search({navigation}){
-    const [subway, setSubway] = useState(null);         // 모든 역 정보
-    const [error, setError] = useState(null);           // 에러 메시지
-    const [searchText, setSearctText] = useState('');   // 검색 결과
+    const [subway, setSubway] = useState(null);         // 모든역정보
+    const [error, setError] = useState(null);           // 에러메시지
+    const [searchText, setSearctText] = useState('');   // 검색결과
 
     // onPressed 할 때 넘어가는 값
-    const [line, setLine] = useState('');       // 연계 호선 명
-    const [stnName, setStnName] = useState(''); // 역 명
-    const [preeStn, setPreeStn] = useState(''); // 이전x2 역
-    const [preStn, setPreStn] = useState('');   // 이전 역
-    const [nextStn, setNextStn] = useState(''); // 다음 역
+    const [line, setLine] = useState('');               // 연계호선명
+    const [stnName, setStnName] = useState('');         // 역명
+    const [preeStn, setPreeStn] = useState('');         // 이전x2역
+    const [preStn, setPreStn] = useState('');           // 이전역
+    const [nextStn, setNextStn] = useState('');         // 다음역
+    const [arvTime,setArvTime] = useState('')           // 도착예정시간
+    const [trnlineNm, setTrnlineNm] = useState('')      // 행-방면
 
+    const [reLine, setReLine] = useState('');
+    const [bookmark, setBookmark] = useState(null); 
 
-    const [bookmark, setBookmark] = useState(null);
-
+    
     useEffect(() => {
         const getSubway = async () => {
             try {
@@ -62,53 +65,75 @@ export default function Search({navigation}){
         return name == word && wordLength != 0;
     }
 
-    // 
+    // 도착지방면 기준으로 중복 값 제거
+    let lineData;
     let uniqData;
     const ChangeText = value => {
         const data = subway.filter( item => { 
             return matchName(item.statnNm, value) == true;
         })
-        uniqData = _.uniqBy(data, "trainLineNm")
+        lineData = _.uniqBy(data, "subwayList")
+        uniqData = _.uniqBy(data, "trainLineNm");
     }
 
-    const postData = () => {
+    const postData = data => {
         console.log('Data extracting..');
+        console.log(data);
 
-        subway.filter(item => {
-            return matchName(item.statnNm, searchText) == true;
-        })
-        .map(item => {
-            return (
-                console.log('444',item.subwayList),
-                console.log('555',item.statnNm),
-                setLine(item.subwayList),
-                setStnName(item.statnNm),
-                subway
-                .filter(v => {
-                    return item.statnTid == v.statnId
-                })
-                .map(v => {
-                    return setNextStn(v.statnNm);
-                }),
-                subway
-                .filter(v => {
-                    return item.statnFid == v.statnId
-                })
-                .map(v => {
-                    return (
-                        setPreStn(v.statnNm),
-                        subway
-                        .filter(e => {
-                            return v.statnFid == e.statnId
-                        })
-                        .map(e => {
-                            return setPreeStn(e.statnNm);
-                        })
-                    )
-                })
-            )
-        })
-        console.log('ppp : ',stnName, preeStn, preStn, nextStn);
+        setLine(data.subwayList);
+
+        console.log('현재역명 : ', data.statnNm)
+        console.log('현재역ID : ', data.statnId) // 현재역 아이디값
+        console.log('도착방면 : ?', data.trainLineNm)
+
+        const statnFid = subway.filter(item => item.statnId === data.statnFid);          // 이전역
+        const statnTid = subway.filter(item => item.statnId === data.statnTid);          // 다음역
+        const preesFid = subway.filter(item => (item.statnId === statnFid[0].statnFid)); // 이전전역
+
+        // console.log('checkout : ', preesFid, statnFid, statnTid);
+
+        setStnName(data.statnNm);           // 현재역
+        setPreeStn(preesFid[0].statnNm);    // 이전x2역
+        setPreStn(statnFid[0].statnNm);     // 이전역
+        setNextStn(statnTid[0].statnNm);    // 다음역
+        setTrnlineNm(data.trainLineNm);     // 행-방면
+        setArvTime(data.barvlDt);           // 도착시간
+
+        // subway.filter(item => {
+        //     return matchName(item.statnNm, searchText) == true;
+        // })
+        // .map(item => {
+        //     return (
+        //         console.log('444',e),
+        //         console.log('555',item.statnNm),
+        //         setLine(e),
+        //         setStnName(item.statnNm),
+        //         subway
+        //         .filter(v => {
+        //             return item.statnTid == v.statnId
+        //         })
+        //         .map(v => {
+        //             return setNextStn(v.statnNm);
+        //         }),
+        //         subway
+        //         .filter(v => {
+        //             return item.statnFid == v.statnId
+        //         })
+        //         .map(v => {
+        //             return (
+        //                 setPreStn(v.statnNm),
+        //                 subway
+        //                 .filter(e => {
+        //                     return v.statnFid == e.statnId
+        //                 })
+        //                 .map(e => {
+        //                     return setPreeStn(e.statnNm);
+        //                 })
+        //             )
+        //         })
+        //     )
+        // })
+        // console.log('ppp : ',stnName, preeStn, preStn, nextStn);
     }
 
     return (
@@ -165,31 +190,87 @@ export default function Search({navigation}){
                                                                     style={styles.result}
                                                                 >
                                                                     {/* 호선 */}
-                                                                    <View style={{ ...styles.listText, flex: 1, flexDirection: "row", alignItems: "center" }}>
-                                                                        <MaterialCommunityIcons
-                                                                            name={`numeric-${item.subwayList[3]}-circle`}
-                                                                            size={16}
-                                                                            color="purple"
-                                                                        />
-                                                                            {
-                                                                                item.subwayList[8] != null
-                                                                                ? (
-                                                                                    <MaterialCommunityIcons
-                                                                                        name={`numeric-${item.subwayList[8]}-circle`}
-                                                                                        size={16}
-                                                                                        color="purple"
-                                                                                    />
-                                                                                )
-                                                                                : null
-                                                                            }
+                                                                    <View style={{...styles.listText, flex: 1, flexDirection: "row", alignItems: "center"}}>
+                                                                        {
+                                                                            lineData.map((e, k) => {
+                                                                                return e.subwayList.split(',')
+                                                                                .map((r, k) => {
+                                                                                    return (
+                                                                                        <Text
+                                                                                            key={k}
+                                                                                            style={{
+                                                                                                ...styles.lines,
+                                                                                                backgroundColor:
+                                                                                                r[2]+r[3] == 1 ? '#0052A4' : (
+                                                                                                    r[2]+r[3] == 2 ? '#009D3E' : (
+                                                                                                        r[2]+r[3] == 3 ? '#EF7C1C' : (
+                                                                                                            r[2]+r[3] == 4 ? '#00A5DE' : (
+                                                                                                                r[2]+r[3] == 5 ? '#996CAC' : (
+                                                                                                                    r[2]+r[3] == 6 ? '#CD7C2F' : (
+                                                                                                                        r[2]+r[3] == 7 ? '#747F00' : (
+                                                                                                                            r[2]+r[3] == 8 ? '#EA545D' : (
+                                                                                                                                r[2]+r[3] == 9 ? '#BB8336' : (
+                                                                                                                                    r[2]+r[3] == 63 ? '#77C4A3' : (
+                                                                                                                                        r[2]+r[3] == 65 ? '#0090D2' : (
+                                                                                                                                            r[2]+r[3] == 67 ? '#0C8E72' : (
+                                                                                                                                                r[2]+r[3] == 75 ? '#F5A200' : (
+                                                                                                                                                    r[2]+r[3] == 77 ? '#D4003B' : (
+                                                                                                                                                        null
+                                                                                                                                                    )
+                                                                                                                                                )
+                                                                                                                                            )
+                                                                                                                                        )
+                                                                                                                                    )
+                                                                                                                                )
+                                                                                                                            )
+                                                                                                                        )
+                                                                                                                    )
+                                                                                                                )
+                                                                                                            )
+                                                                                                        )
+                                                                                                    )
+                                                                                                )
+                                                                                            }}
+                                                                                        >
+                                                                                        {
+                                                                                            r[2] == 0
+                                                                                            ? r[3]
+                                                                                            : (
+                                                                                                r[2]+r[3] == 63
+                                                                                                ? '경'
+                                                                                                : (
+                                                                                                    r[2]+r[3] == 65
+                                                                                                    ? '공'
+                                                                                                    : (
+                                                                                                        r[2]+r[3] == 67
+                                                                                                        ? '경'
+                                                                                                        : (
+                                                                                                            r[2]+r[3] == 75
+                                                                                                            ? '수'
+                                                                                                            : (
+                                                                                                                r[2]+r[3] == 77
+                                                                                                                ? '신'
+                                                                                                                : null
+                                                                                                            )
+                                                                                                        )
+                                                                                                    )
+                                                                                                )
+                                                                                            )
+                                                                                        }
+                                                                                        </Text>
+                                                                                    )
+                                                                                })
+                                                                            })
+                                                                        }
                                                                     </View>
-
+                                                                    
+                                                                    {/* 역 명 */}
                                                                     <TouchableOpacity
                                                                         style={{ flex: 2 }}
-                                                                        onPressIn={() => { postData() }}
+                                                                        onPressIn={() => { postData(uniqData[k]) }}
                                                                         onPressOut={() => {
                                                                             navigation.navigate('실시간 역 정보', {
-                                                                                line, stnName, preeStn, preStn, nextStn,
+                                                                                line, stnName, preeStn, preStn, nextStn, arvTime, trnlineNm
                                                                             })
                                                                         }}
                                                                     >
@@ -293,20 +374,6 @@ const styles = StyleSheet.create({
         alignItems:"center",
         borderRadius:10,
     },
-    Loading:{
-        flex:1,
-        marginTop:100,
-        marginBottom:200,
-        marginHorizontal:30,
-        justifyContent:"center",
-        alignItems:"center",
-        borderWidth:1,
-        borderColor:"#000",
-    },
-    LoadingText:{
-        fontSize:20,
-        color:"#777",
-    },
     resultBox:{
         flex:1,
         marginHorizontal:20,
@@ -323,6 +390,14 @@ const styles = StyleSheet.create({
     ctgText:{
         fontWeight:"bold",
     },
+    lines:{
+        marginRight:1,
+        paddingHorizontal:2,
+        paddingVertical:1,
+        fontSize:8,
+        color:"#fff",
+        borderRadius:20,
+    },
     resultList:{
         flex:10,
         alignItems:"center",
@@ -336,5 +411,21 @@ const styles = StyleSheet.create({
     },
     listText:{
         justifyContent:"center",
-    }
+    },
+
+     /* loading */
+     Loading:{
+        flex:1,
+        marginTop:100,
+        marginBottom:200,
+        marginHorizontal:30,
+        justifyContent:"center",
+        alignItems:"center",
+        borderWidth:1,
+        borderColor:"#000",
+    },
+    LoadingText:{
+        fontSize:20,
+        color:"#777",
+    },
 });
